558a69dc1ad45b5b3fb429f1f58eb1382e65c963
Nicolas A. Tonne <nixatto@gmail.com>
2015-10-21 22:55:35 +0200

    Nibble app

--- app.js
+++ app.js
@@ -9,18 +9,13 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/'});
 }]).
 controller('MainCtrl', ["$scope", function ($scope) {
-        $scope.select = {
-            value1: "Option1",
-            value2: "I'm an option",
-            choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
-        };
+    $scope.select = {
+        value1: "Option1",
+        value2: "I'm an option",
+        choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
+    };
 
-        $scope.dummyInputs = {};
+    $scope.dummyInputs = {};
 
-    }]);
-
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.login'
-])
+}])
 ;
