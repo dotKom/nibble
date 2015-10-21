e917e7684df33b75276f78aed386cb12e47556e2
Nicolas A. Tonne <nixatto@gmail.com>
2015-10-21 22:47:19 +0200

    Rename app and structure

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-'use strict';
-
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('nibble', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ui.materialize',
+  'nibble.view1',
+  'nibble.view2',
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}]).
+controller('MainCtrl', ["$scope", function ($scope) {
+        $scope.select = {
+            value1: "Option1",
+            value2: "I'm an option",
+            choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
+        };
+
+        $scope.dummyInputs = {};
+
+    }]);
