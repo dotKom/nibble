9f42c71abbc48b6079c3f8cce3b96ebdab2d9cd6
Nicolas A. Tonne <nixatto@gmail.com>
2015-10-28 20:48:13 +0100

    Added items in shop, removed spaces in html attributes, stuff

--- views/shop/shop.js
+++ views/shop/shop.js
@@ -9,6 +9,29 @@ angular.module('nibble.shop', ['ngRoute'])
   });
 }])
 
-.controller('shopCtrl', [function() {
+.controller('shopCtrl', function($scope) {
+    var testItem = {"name":"Øl", "description":"0.5L Dahls på glassflaske", "price":"20", "amount":42, "available":true, "category":"drink"}
+    var testItem2 = {"name":"Billys", "description":"Dypfryst pizza med ost og skinke", "price":"20", "amount":42, "available":true, "category":"mat"}
+    
+    $scope.items = [testItem, testItem2]
+})
+;
 
-}]);
+/*
+=== Models ===
+
+Product
+    Name
+    Price
+    Description
+    Amount
+    Available (If the product should be displayed to the user)
+    Product Category
+
+Transaction
+    User
+    Money diff (minus for subtraction, plus for addition) (Optional)
+    Product (Optional)
+    Timestamp
+
+*/
