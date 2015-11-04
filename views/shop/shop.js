2ae14adf02b1efa3607612f64b0e02778835ef9d
Jarle Trollebø <aborysa@gmail.com>
2015-11-04 22:08:41 +0100

    *Added checkout function to shop.js

--- views/shop/shop.js
+++ views/shop/shop.js
@@ -10,28 +10,62 @@ angular.module('nibble.shop', ['ngRoute'])
 }])
 
 .controller('shopCtrl', function($scope, $rootScope, $location, $http) {
-
-  if(!$scope.user){
+  /*Note: The controller runs tiwce for some reason*/
+  if(!$rootScope.user){
     $rootScope.user = {name: "testUser",balance: 200};
     /*$location.url("/");
     Materialize.toast("You need to login first!", 4000);
     return;*/
   }
-
-  var testItem = {"name":"Øl", "description":"0.5L Dahls på glassflaske", "price":"20", "amount":42, "available":true, "category":"drink", 
+  $http({
+    url: "/api/v1/inventory/",
+    method: "post",
+    data: {}
+  }).then(function(ret){
+    $scope.items = ret.data.inventory;
+  },function(error){
+    Materialize.toast("Could not load shop inventory", 4000);
+  });
+  
+  var testItem = {id:"1","name":"Øl", "description":"0.5L Dahls på glassflaske", "price":"20", "amount":42, "available":true, "category":"drink", 
                   "image": "http://3.bp.blogspot.com/_eBUfxxSLsVw/TSoQTbARxiI/AAAAAAAAAEk/V927sCd8uRU/s1600/dahls.png","dispCount":0}
-  var testItem2 = {"name":"Billys", "description":"Dypfryst pizza med ost og skinke", "price":"20", "amount":42, "available":true, 
+  var testItem2 = {id:"2","name":"Billys", "description":"Dypfryst pizza med ost og skinke", "price":"20", "amount":42, "available":true, 
                   "category":"mat", "image": "http://www.brynildsen.no/upload/Billys-original-NY.png","dispCount":0}
   
-  $scope.items = [testItem, testItem2, testItem, testItem2, testItem, testItem2, testItem, testItem2]
+  $scope.items = [testItem, testItem2, testItem, testItem2, testItem, testItem2, testItem, testItem2];
   $scope.changeCount = function(itemRef,count){
-    if(!$scope.shopQueue[itemRef.name]){
-      $scope.shopQueue[itemRef.name] = {"item":itemRef,"count": 0}
+    if(!$scope.shopQueue[itemRef.id]){
+      $scope.shopQueue[itemRef.id] = {"item":itemRef,"count": 0};
     }
-    $scope.shopQueue[itemRef.name].count = Math.max(0,$scope.shopQueue[itemRef.name].count+count);
-    $scope.shopQueue[itemRef.name].item.dispCount = $scope.shopQueue[itemRef.name].count;
+    $scope.shopQueue[itemRef.id].count = Math.max(0,$scope.shopQueue[itemRef.id].count+count);
+    $scope.shopQueue[itemRef.id].item.dispCount = $scope.shopQueue[itemRef.id].count;
+    /*if($scope.shopQueue[itemRef.id].count <= 0){
+      $scope.shopQueue[itemRef.id] = undefined;
+    }*/
   }
   $scope.shopQueue = {};
+  $scope.checkout = function(){
+    /*
+      Insert validation code here  
+    */
+     http({
+      url: "/payme/buy",
+      method: "post",
+      data: {
+        "user": $rootScope.rfid,
+        "products": $scope.shopQueue
+      }
+     }).then(function(ret){
+       $scope.shopQueue = {};
+       /*Prompt user to 'logout' or 'continue shopping'*/
+     },function(error){
+       Materialize.toast("Could not checkout", 4000);
+       Materialize.toast("Server returned error code: " + error.status, 4000);
+        
+     });
+  }
+  
+  
   /*function add(item){
     try:
     order[item.unique].amount += 1;
