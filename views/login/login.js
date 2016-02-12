8516ad2f6f06e4b1396a5ffda0050c21da38c2d2
Jarle Trollebø <aborysa@gmail.com>
2016-02-12 02:08:14 +0100

    Removed console logging and added some simple rfid validation."

--- views/login/login.js
+++ views/login/login.js
@@ -10,48 +10,48 @@ angular.module('nibble.login', ['ngRoute'])
 }])
 .controller('loginCtrl', ["$rootScope","$http","$location","$scope","api.config","User",function(root,http,location,scope,api,User) {
   window.logKeys = true;
-  window.rfid = ""
+  window.rfid = "";
   scope.regModal = false;
-  scope.rfid = "";
+  //In root due to being accessed by a modal in index.html
   root.submit_reg = function(){
-    http({
-      url: api.apiRoot + "rfid/",
-      method: "POST",
-      data: {
-        username: $("#user-username").val(),
-        password: $("#user-password").val(),
-        rfid: root.rfid
-      }
-    }).then(function(ret){
-      /*Success*/
-      scope.submit_login();
-      $('#regModal').closeModal();
-      
-    },function(error){
-      console.log(error);
-      Materialize.toast("Registrering feilet!",3000);
-    });
+    if(isValidRFID(root.rfid)){
+      http({
+        url: api.apiRoot + "rfid/",
+        method: "POST",
+        data: {
+          username: $("#user-username").val(),
+          password: $("#user-password").val(),
+          rfid: root.rfid
+        }
+      }).then(function(ret){
+        /*Success*/
+        scope.submit_login();
+        $('#regModal').closeModal();
+
+      },function(error){
+        console.log(error);
+        Materialize.toast("Registrering feilet!",3000);
+      });
+    }
   }
   scope.submit_login = function(){
-      /*Validation and 'login' code:*/
+    /*Validation and 'login' code:*/
+    $("#rfid-rlogo")[0].style.borderColor = "#65EC00";
     window.logKeys = false;
     root.rfid = window.rfid;
-    //window.rfid = "";
-	console.log("rdif cleared: " + window.rfid);
     root.validation_fail = false;
+    if(isValidRFID(root.rfid)){
       http({
         url: api.apiRoot + "usersaldo/?format=json&rfid="+ root.rfid,
         method: "get"
       }).then(function(ret){
           if(ret.data.count == 1){
             $("#rfid-rlogo")[0].style.borderColor = "#65EC00";
-            root.user = ret.data.results[0]; //<-- if data is json??
+            root.user = ret.data.results[0];
             root.user.balance = root.user.saldo;
             root.user.name = root.user.first_name + " " + root.user.last_name;
             location.url("/shop");
             window.rfid = "";
-            console.log(root.user);
-            console.log(ret.data);
           }
           else{
             $("#rfid-rlogo")[0].style.borderColor = "#FF471E";
@@ -63,8 +63,8 @@ angular.module('nibble.login', ['ngRoute'])
               "complete": function(){
                 $("#rfid-rlogo")[0].style.borderColor = "";
                 window.logKeys = true;
-                window.rfid = "";
-	            $("#user-username").val("");
+                window.rfid = null;
+                $("#user-username").val("");
                 $("#user-password").val("");  
                 scope.regModal = false;
               }
@@ -88,7 +88,7 @@ angular.module('nibble.login', ['ngRoute'])
         }
       );
     };
-    
+  }
   /*
   Item list should be loaded in app to be used by both shop and login (?)
   */
@@ -96,13 +96,17 @@ angular.module('nibble.login', ['ngRoute'])
     root.itemCols = [root.items.slice(0, Math.ceil(root.items.length/2)), root.items.slice(Math.ceil(root.items.length/2))]
   }
 }]);
- 
+function isValidRFID(rfid){
+  //Null check
+  if(typeof(rfid) === "string"){
+    return /^\d{8,10}$/.test(rfid);
+  }
+  return false;
+}
 $('body').keypress(function(key) {
   /*Logs keys when in login and not regestering*/
   if(window.logKeys){
     if(key.keyCode == 13){ // Enter
-      console.log(window.rfid);
-      $("#rfid-rlogo")[0].style.borderColor = "#65EC00";
       $("#rfid-form").submit();
     }
     else{
